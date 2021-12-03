77358f073ebc67a0bbf4f50b07f47335bbff9a19
rpcmoreira <82443429+rpcmoreira@users.noreply.github.com>
2021-12-03 15:13:28 +0000

    Add files via upload
    Codigo Updated com random number generator a criar 4 numeros aleatorios e depois substituindo por letras.

--- Mastermind.asm
+++ Mastermind.asm
@@ -1,6 +1,8 @@
 .data
 .align 1
-cores: .ascii "B","G","R","W","B","O"			#Azul, Verde, Vermelho, Branco, Preto, Laranja
+cores: .ascii "BGRWPO"			#Azul, Verde, Vermelho, Branco, Preto, Laranja
+.align 1
+code_let: .space 4
 
 .align 4
 code: .space 12
@@ -8,10 +10,11 @@ code: .space 12
 
 
 
+
 .text
 
 rand_4: li $t0, 0
-loop:   beq $t0, 16, end_loop
+loop:   beq $t0, 16, codigo_troca
 	li $v0, 42
 	li $a1,	5
 	syscall
@@ -19,18 +22,41 @@ loop:   beq $t0, 16, end_loop
 	sw $a0, code($t0)
 	addi $t0, $t0, 4
 	j loop
+
+
+codigo_troca:		
+	la $s2, code_let
+	li $s7, 0
+codigo_letras: 
+	beq $s7, 16, end_loop
+	la $s0, code
+	la $s1, cores
 	
-end_loop:li $t1, 0
+	lw $t1, code($s7)
+	add $s1, $s1, $t1
+	lb $t2, ($s1)
+	
+	sb $t2, ($s2)
+	
+	addi $s7, $s7, 4
+	addi $s2, $s2, 1
+	j codigo_letras
+
+
+end_loop:
+	li $t1, 0
+	la $t0, code_let
 loop2:
-	beq $t1, 16, end
+	beq $t1, 4, end
 	
-	lw $a0, code($t1)
-	li $v0, 1
+	lb $t3, ($t0)
+	add $a0, $t3, $0
+	li $v0, 11
 	syscall
 	
-	addi $t1, $t1, 4
+	addi $t0, $t0, 1
+	addi $t1, $t1, 1
 	j loop2
-	
 	
 end: li $v0,10
      syscall
