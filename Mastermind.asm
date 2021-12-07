d88b201e7c64bde50e8d81b3778aced421d497ff
rpcmoreira <82443429+rpcmoreira@users.noreply.github.com>
2021-12-07 10:58:44 +0000

    Mastermind New
    Verificacao do codigo parcialmente implementada, com a msg de erro tambem, faz a comparacao correta
    No entanto, a msg de erro nao aparece quando deve, e esta a usar na mesma uma tentativa ao user, mesmo estando invalido

--- Mastermind.asm
+++ Mastermind.asm
@@ -3,7 +3,7 @@ MSG: .asciiz "\nInsira a tua tentativa de descodificação:\n"
 BR: .asciiz "\n"
 MSG2: .asciiz "\nPontuação: \n"
 MSG3: .asciiz "\nContinuar o jogo? 'e' para sair do jogo.\n"
-MSG_INVALIDO: .asciiz "\nCarater invalido!\n"
+MSG_INVALIDA: .asciiz "\nCarater invalido!\n"
 end_letter: .asciiz "e\n"
 
 end_input: .space 1
@@ -27,6 +27,7 @@ code: .space 12				#Array onde estara o codigo gerado pelo random_4
 
 .text
 
+############################################################################ INICIO JOGO E RANDOMIZER DO CODIGO
 rand_4: li $t0, 0
 loop:   beq $t0, 16, codigo_troca
 	li $v0, 42
@@ -55,9 +56,9 @@ codigo_letras:
 	addi $s7, $s7, 4
 	addi $s2, $s2, 1
 	j codigo_letras
+###############################################################################################################
 
-
-
+######################################################################################### INICIO DO JOGO JOGADO
 main: 	
 	la $t0, tentativas
 	li $t2, 0
@@ -72,9 +73,9 @@ main_loop:
 	li $v0,8
 	syscall
 	
-	li $t3, 0
+	jal loop_3
 	
-
+	li $t3, 0
 loop_tent:
 	beq $t3, 4, end_loop_tent	
 	lb $a0, buffer($t3)
@@ -88,7 +89,42 @@ end_loop_tent:
 	addi $t2, $t2, 4
 	j main_loop
 	
+		
+#######################################################################################################################
+# VERIFICAÇAO DO CODIGO INSERIDO (NOT WORKING AS INTENDED)	
+verificacao_cod:
+	add $s7, $0, $0
+	la $t9, cores
+loop_3: lb $t8, buffer($s4)
+loop_3_1:
+	beq $s4, 4, end_l3_c  
+loop_3_2:
+	lb $s6, cores($t9)
+	beq $t9, 6, end_l3_e
+	beq $s6, $t8, end_l3_e			#Comparacao e bem sucedida se ele for correto
+	beq $t9, 6, sim
+	
+	addi $t9, $t9, 1
+	j loop_3_2
+	
+	
+sim: 	la $a0, MSG_INVALIDA
+	li $v0, 4
+	syscall
+	
+	jr $ra
+	
+end_l3_c:
+	addi $s5, $0, 100
+	jr $ra
+	
+end_l3_e: 
+	add $t9, $0, $0
+	addi $s4, $s4, 1
+	j loop_3
+#########################################################################################################################
 
+############################################################################################################# FIM DO JOGO
 end_loop:
 	li $t1, 0
 	la $t0, code_let
@@ -116,13 +152,13 @@ end:
      
      li $v0, 8
      la $a0, end_input
-     syscall # Resposta do usuario
-     move $t0, $a0 # Grava a resposta em $t0
+     syscall 			# Resposta do usuario
+     move $t0, $a0 		# Grava a resposta em $t0
      
      la $t2, end_letter 
      lb $t3, ($t0)
      lb $t4, ($t2)
-     bne $t3, $t4, rand_4 # Se não escrever 'e' o jogo continua
+     bne $t3, $t4, rand_4 	# Se não escrever 'e' o jogo continua
 
      li $v0,10
      syscall
